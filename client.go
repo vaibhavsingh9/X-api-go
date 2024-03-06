7b3d47e9ce5806db2bd50a81f99caafd64278a87
vaibhav singh <vaibhav0001singh@gmail.com>
2024-03-06 21:38:34 +0530

    added all functionalities

--- client.go
+++ client.go
@@ -11,13 +11,38 @@ import (
 	"github.com/dghubble/oauth1"
 )
 
+// Struct to parse webhook load
+type WebhookLoad struct {
+	UserId           string  `json:"for_user_id"`
+	TweetCreateEvent []Tweet `json:"tweet_create_events"`
+}
+
+// Struct to parse tweet
+type Tweet struct {
+	Id    int64
+	IdStr string `json:"id_str"`
+	User  User
+	Text  string
+}
+
+// Struct to parse user
+type User struct {
+	Id     int64
+	IdStr  string `json:"id_str"`
+	Name   string
+	Handle string `json:"screen_name"`
+}
+
 func CreateClient() *http.Client {
 	//Create oauth client with consumer keys and access token
 	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
 	token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN_KEY"), os.Getenv("ACCESS_TOKEN_SECRET"))
 
+	subscribeWebhook()
 	return config.Client(oauth1.NoContext, token)
+
 }
+
 func registerWebhook() {
 	fmt.Println("Registering webhook...")
 	httpClient := CreateClient()
@@ -37,4 +62,19 @@ func registerWebhook() {
 		panic(err)
 	}
 	fmt.Println("Webhook id of " + data["id"].(string) + " has been registered")
+}
+
+func subscribeWebhook() {
+	fmt.Println("subscribing webapp...")
+	client := CreateClient()
+	path := "https://api.twitter.com/1.1/account_activity/all/" + os.Getenv("WEBHOOK_ENV") + "/subscriptions.json"
+	resp, _ := client.PostForm(path, nil)
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if resp.StatusCode == 204 {
+		fmt.Println("subscribed successfully")
+	} else if resp.StatusCode != 204 {
+		fmt.Println(("could not subscribe the webhook"))
+		fmt.Println(string(body))
+	}
 }
--- hellobot.go
+++ hellobot.go
@@ -8,8 +8,10 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -31,7 +33,7 @@ func main() {
 		fmt.Fprintf(writer, "server is up and running")
 	})
 	m.HandleFunc("/webhook/twitter", CrcCheck).Methods("GET")
-
+	m.HandleFunc("/twitter/webhook", WebhookHandler).Methods("POST")
 	server := &http.Server{
 		Handler: m,
 	}
@@ -53,4 +55,54 @@ func CrcCheck(writer http.ResponseWriter, request *http.Request) {
 	response["response_token"] = "sha256" + encoded
 	responseJson, _ := json.Marshal(response)
 	fmt.Fprintf(writer, string(responseJson))
+}
+
+func WebhookHandler(writer http.ResponseWriter, request http.Request) {
+	fmt.Println("Handler called")
+	//Read the body of the tweet
+	body, _ := ioutil.ReadAll(request.Body)
+	//Initialize a webhok load obhject for json decoding
+	var load WebhookLoad
+	err := json.Unmarshal(body, &load)
+	if err != nil {
+		fmt.Println("An error occured: " + err.Error())
+	}
+	//Check if it was a tweet_create_event and tweet was in the payload and it was not tweeted by the bot
+	if len(load.TweetCreateEvent) < 1 || load.UserId == load.TweetCreateEvent[0].User.IdStr {
+		return
+	}
+	//Send Hello world as a reply to the tweet, replies need to begin with the handles
+	//of accounts they are replying to
+	_, err = SendTweet("@"+load.TweetCreateEvent[0].User.Handle+" Hello World", load.TweetCreateEvent[0].IdStr)
+	if err != nil {
+		fmt.Println("An error occured:")
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Tweet sent successfully")
+	}
+}
+
+func SendTweet(tweet string, reply_id string) (*Tweet, error) {
+	fmt.Println("Sending tweet as reply to " + reply_id)
+	//Initialize tweet object to store response in
+	var responseTweet Tweet
+	//Add params
+	params := url.Values{}
+	params.Set("status", tweet)
+	params.Set("in_reply_to_status_id", reply_id)
+	//Grab client and post
+	client := CreateClient()
+	resp, err := client.PostForm("https://api.twitter.com/1.1/statuses/update.json", params)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	//Decode response and send out
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	err = json.Unmarshal(body, &responseTweet)
+	if err != nil {
+		return nil, err
+	}
+	return &responseTweet, nil
 }
